b118719897884ddc6372e9fcb4b7bda117f147f9
Hung Nguyen <hung.6.nguyen@aalto.fi>
2023-11-09 12:09:35 +0200

    Update websire

--- project-files/web_interface/main.py
+++ project-files/web_interface/main.py
@@ -7,7 +7,7 @@ import os
 from func_import import open_file
 
 st.set_page_config(layout="wide")
-st.title("Biological dashboard")
+st.title("Biology dashboard")
 
 st.markdown(
     """
@@ -40,3 +40,35 @@ with tab_biod:
             width=1200,
             height=800,
         )
+
+with tab_rela:
+    tab_rela.header("Relationships")
+    tab_rela.subheader("Parastitic")
+    tab_rela._html(
+        open_file(f"./precomp_data/correlation/parasitic_bar.html")[1],
+        width=1400,
+        height=500,
+    )
+    tab_rela.write(
+        "In parasitic relationships, one specie is dependent on the other one to live and by that it harms it as well. In this case, we are looking at Taphrina betulina and Betula pubescens, the first one is a fungus and the other one is a tree. From the graph, it is clear that the fungus almost exclusively lives in the locations that has the tree."
+    )
+
+    tab_rela.subheader("Predatory")
+    tab_rela._html(
+        open_file(f"./precomp_data/correlation/predatory_bar.html")[1],
+        width=1400,
+        height=500,
+    )
+    tab_rela.write(
+        "In predatory relationships, one specie hunts and eats the other one. In this example case, we are looking to Aegolius funereus vs. Clethrionomys glareolus & Microtus agrestis, which are an owl and two moles that owl primarily hunts. It is clear from the graphs that owls appear more in the locations that has moles."
+    )
+
+    tab_rela.subheader("Symbiotic")
+    tab_rela._html(
+        open_file(f"./precomp_data/correlation/symbiotic_bar.html")[1],
+        width=1400,
+        height=500,
+    )
+    tab_rela.write(
+        "Symbiosis is a type of relationship between two different biological species where one specie gets benefit of other one to sustain its life, however, the specie that provides the benefits doesn't get affected by that. In this case, we are looking to Amanita muscaria and Pinus sylvestris where Amantia muscaria grows more in locations that has Pinus sylvetris."
+    )
