2cec75b843d16b22a8f0f071d57cf33f7e121f7a
Hung Nguyen <hung.6.nguyen@aalto.fi>
2023-11-14 21:07:18 +0200

    Update relationship plots. Add a new project. Remove redundant files in web_interface.

--- project-files/web_interface/src/components/project_card.py
+++ project-files/web_interface/src/components/project_card.py
@@ -30,10 +30,26 @@ def render(file_path, title, text, plot, key, file_name: str = "download.csv"):
         if title == "Climate change and butterflies":
             col1, col2 = st.columns([0.85, 0.25])
             year = str(col2.slider("Year slider", 2000, 2008, 2000))
-            col2.write(f"Showing data from 2000 to {year}.")
+            col2.caption(f"Showing data from 2000 to {year}")
             col1._html(
                 open_file(f"./precomp_data/grid_map/butterfly_{year}.html")[1],
                 height=700,
             )
+
+        elif title == "Interspecies dynamics":
+            vis_type = st.radio(
+                "Visualizations", ["Map box", "Bar chart"], horizontal=True
+            )
+            if vis_type == "Map box":
+                components.html(
+                    open_file(f"./precomp_data/projects/interrelation_map.html")[1],
+                    height=700,
+                )
+            else:
+                components.html(
+                    open_file(f"./precomp_data/projects/interrelation_bar.html")[1],
+                    height=500,
+                )
+
         else:
             components.html(plot, height=500)
--- project-files/web_interface/src/relationship_tab.py
+++ project-files/web_interface/src/relationship_tab.py
@@ -8,37 +8,65 @@ sys.dont_write_bytecode = True
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(CURRENT_DIR))
 from func_import import open_file
-from func_import import open_file
 
 
 def render():
     st.header("Relationships")
-    st.subheader("Parastitic")
-    components.html(
-        open_file(f"./precomp_data/correlation/parasitic_bar.html")[1],
-        height=500,
-    )
-    st.write(
-        "In parasitic relationships, one specie is dependent on the other one to live and by that it harms it as well. In this case, we are looking at Taphrina betulina and Betula pubescens, the first one is a fungus and the other one is a tree. From the graph, it is clear that the fungus almost exclusively lives in the locations that has the tree."
-    )
+    col1, col2 = st.columns([0.55, 0.45], gap="medium")
+    relationship = "Symbiotic"
+    with col1:
+        relationship = st.selectbox(
+            "Relationship type", ["Symbiotic", "Predatory", "Parasitic"]
+        )
+        if relationship == "Symbiotic":
+            st.markdown(
+                """
+                        Symbiosis is a type of relationship between two different biological species where one specie gets benefit of other one to sustain its life, however, the specie that provides the benefits doesn't get affected by that.
 
-    st.subheader("Predatory")
-    components.html(
-        open_file(f"./precomp_data/correlation/predatory_bar.html")[1],
-        height=500,
-    )
-    st.write(
-        "In predatory relationships, one specie hunts and eats the other one. In this example case, we are looking to Aegolius funereus vs. Clethrionomys glareolus & Microtus agrestis, which are an owl and two moles that owl primarily hunts. It is clear from the graphs that owls appear more in the locations that has moles."
-    )
+                        In this case, we are looking to Amanita muscaria and Pinus sylvestris where Amantia muscaria grows more in locations that has Pinus sylvetris.
+                        """
+            )
 
-    st.subheader("Symbiotic")
-    components.html(
-        open_file(f"./precomp_data/correlation/symbiotic_bar.html")[1],
-        height=500,
-    )
-    st.write(
-        "Symbiosis is a type of relationship between two different biological species where one specie gets benefit of other one to sustain its life, however, the specie that provides the benefits doesn't get affected by that. In this case, we are looking to Amanita muscaria and Pinus sylvestris where Amantia muscaria grows more in locations that has Pinus sylvetris."
-    )
+        elif relationship == "Predatory":
+            st.markdown(
+                """
+                        In predatory relationships, one specie hunts and eats the other one. In this example case, we are looking to Aegolius funereus vs. Clethrionomys glareolus & Microtus agrestis, which are an owl and two moles that owl primarily hunts. 
+
+                        It is clear from the graphs that owls appear more in the locations that has moles.
+                        """
+            )
+
+        else:
+            st.markdown(
+                """
+                        In parasitic relationships, one specie is dependent on the other one to live and by that it harms it as well. In this case, we are looking at Taphrina betulina and Betula pubescens, the first one is a fungus and the other one is a tree. 
+
+                        From the graph, it is clear that the fungus almost exclusively lives in the locations that has the tree.
+                        """
+            )
+
+        components.html(
+            open_file(f"./precomp_data/correlation/{relationship}_bar.html")[1],
+            height=500,
+        )
+    with col2:
+        if relationship == "Symbiotic":
+            components.html(
+                open_file(f"./precomp_data/correlation/{relationship}_map.html")[1],
+                height=700,
+            )
+
+        elif relationship == "Predatory":
+            components.html(
+                open_file(f"./precomp_data/correlation/{relationship}_map.html")[1],
+                height=700,
+            )
+
+        else:
+            components.html(
+                open_file(f"./precomp_data/correlation/{relationship}_map.html")[1],
+                height=700,
+            )
 
 
 if __name__ == "__main__":
